3711cb7ad8b33d7671bfc899dd1234cf5bb03550
korea7030 <korea7030@naver.com>
2020-11-18 23:25:13 +0900

    elasticbeanstalk deploy

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = env_variable['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'https://9584qko1ia.execute-api.ap-northeast-2.amazonaws.com/dev']
+ALLOWED_HOSTS = ['*',
+     'https://9584qko1ia.execute-api.ap-northeast-2.amazonaws.com/dev', 
+     'django-env.eba-ph2wcqgn.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
